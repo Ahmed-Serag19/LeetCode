4092b7c962eca81de436efcde9755ec8efefda38
Ahmed Mohamed <80613530+Ahmed-Serag19@users.noreply.github.com>
2024-08-11 05:20:00 +0300

    Time: 66 ms (54.22%), Space: 49.4 MB (59.20%) - LeetHub

---
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -0,0 +1,42 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+function cancellable(fn, args, t) {
+    let timeoutId;
+
+    // Set a timeout to execute the function after `t` milliseconds
+    timeoutId = setTimeout(() => {
+        fn(...args); // Execute the function with the provided arguments
+    }, t);
+
+    // Return the cancel function
+    return function cancelFn() {
+        clearTimeout(timeoutId); // Cancel the execution if the cancel function is called
+    };
+}
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
