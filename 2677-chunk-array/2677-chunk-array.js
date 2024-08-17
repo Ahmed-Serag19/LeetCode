a34844fe49de7e8d523f83a12ebf5d707c4e4a76
Ahmed Mohamed <80613530+Ahmed-Serag19@users.noreply.github.com>
2024-08-17 19:31:34 +0300

    Time: 50 ms (83.37%), Space: 50.9 MB (91.60%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+  
+    const chunked = [];
+    for (let i = 0; i < arr.length; i += size) {
+        chunked.push(arr.slice(i, i + size));
+    }
+    return chunked;
+
+};
