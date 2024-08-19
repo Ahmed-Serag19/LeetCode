e6b8fb38a367f2ad2738ea2d6a16ee3b9a210b29
Ahmed Mohamed <80613530+Ahmed-Serag19@users.noreply.github.com>
2024-08-19 14:52:51 +0300

    Time: 56 ms (24.81%), Space: 48.8 MB (40.36%) - LeetHub

--- 2726-calculator-with-method-chaining/2726-calculator-with-method-chaining.js
+++ 2726-calculator-with-method-chaining/2726-calculator-with-method-chaining.js
@@ -1,43 +1,28 @@
 class Calculator {
-    
-    /** 
-     * @param {number} value
-     */
+
     constructor(value) {
         this.result = value;
     }
     
-    /** 
-     * @param {number} value
-     * @return {Calculator}
-     */
+
     add(value) {
         this.result += value;
         return this;
     }
     
-    /** 
-     * @param {number} value
-     * @return {Calculator}
-     */
+
     subtract(value) {
         this.result -= value;
         return this;
     }
     
-    /** 
-     * @param {number} value
-     * @return {Calculator}
-     */  
+ 
     multiply(value) {
         this.result *= value;
         return this;
     }
     
-    /** 
-     * @param {number} value
-     * @return {Calculator}
-     */
+
     divide(value) {
         if (value === 0) {
             throw new Error("Division by zero is not allowed");
@@ -46,18 +31,13 @@ class Calculator {
         return this;
     }
     
-    /** 
-     * @param {number} value
-     * @return {Calculator}
-     */
+
     power(value) {
         this.result = Math.pow(this.result, value);
         return this;
     }
     
-    /** 
-     * @return {number}
-     */
+ 
     getResult() {
         return parseFloat(this.result.toFixed(5));
     }
